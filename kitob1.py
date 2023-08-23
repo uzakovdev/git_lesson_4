410aa3746df560f056f24d31cefe5ba7bf62bb25
Uzakov Elbek <elbekuzokov7600@gmail.com>
2023-08-24 03:55:22 +0500

    change kitob1

---
+++ kitob1.py
@@ -0,0 +1,2 @@
+# “1 Shohlar” va “2 Shohlar”ning asl nusxasi bitta ibroniycha kitob bo‘lib, ikkala qismini bitta
+# o‘ramga joylash uzunlik qilganidan, kitob ikki qismga — “1 Shohlar” va “2 Shohlar”ga bo‘lingan.
